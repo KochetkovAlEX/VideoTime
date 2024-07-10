79ca27b2401d4e1ddad4cd811d4e78347d09f425
Alex <leshakochetcov@gmail.com>
2024-07-10 17:59:56 +0300

    Шаблоны

---
+++ DJ/mysite/tiktok/template_name.py
@@ -0,0 +1,6 @@
+main_page_template = 'index.html'
+login_page_template = 'login.html'
+reg_page_template = 'reg_page.html'
+reg_page_done_template = 'reg_done.html'
+upload_video_template = 'upload_video.html'
+user_page_template = 'userpage.html'
